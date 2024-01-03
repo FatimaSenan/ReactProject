14cbfab8b9e5a5b183ecebf710633230b07123d2
FatimaSenan <senanfatima1@gmail.com>
2024-01-03 13:28:32 +0100

    second commit

---
+++ travel/src/components/Contact.js
@@ -0,0 +1,16 @@
+import FirstSection from "./FirstSection";
+import Navbar from "./Navbar";
+import { ContacForm } from "./ContacForm";
+
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <>
+      
+      <ContacForm/>
+      
+
+    </>
+  )
+}
