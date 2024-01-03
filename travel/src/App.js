63d03930cd1bcb8f8c7ddc1b69fe109a64eddd36
Ibtissameee <129773647+Ibtissameee@users.noreply.github.com>
2024-01-03 22:22:02 +0100

    last version app.css app.js index.js

--- travel/src/App.js
+++ travel/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
-import Login from './components/pages/Login';
 import { SignUp } from './components/SignUp';
+import Offer from './components/Offer';
+import Offers from './components/pages/Offers';
 import { ContactUs } from './components/pages/ContactUs';
 import { About } from './components/pages/About';
-
+import Login from './components/pages/Login';
 
 function App() {
   return (
@@ -15,11 +16,11 @@ function App() {
       <Navbar  />
       <Routes>
         <Route path='/' exact Component={Home}/> 
+        <Route path="/offer" exact Component={Offer}/>
+        <Route path="/Offers" exact Component={Offers}/>
         <Route path='/sign-up' Component={Login} />
         <Route path='/contact' Component={ContactUs} />
         <Route path='/about' Component={About} />
-
-        
       </Routes>
       
       </Router>
